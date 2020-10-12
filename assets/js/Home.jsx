fa2794a42bd22b8260b6efa15f98d2a5857da833
Abe Estrada <abraham.estrada@gmail.com>
2020-10-12 13:52:52 -0600

    Update preact example

--- assets/js/Home.jsx
+++ assets/js/Home.jsx
@@ -1,8 +1,17 @@
 /** @jsx h */
 import { h } from "preact";
+import { useState } from "preact/hooks";
 
 const Home = () => {
-    return <div>Coming soon.</div>;
+    const [name, setName] = useState("world");
+    return (
+        <div>
+            <p>Hello {name}!</p>
+            <p>
+                <input type="text" value={name} onInput={(e) => setName(e.target.value)} />
+            </p>
+        </div>
+    );
 };
 
 export default Home;
